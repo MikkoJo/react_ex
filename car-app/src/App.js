cd97a0c35c6a5966b2e41da7055bb67f8a6b4754
Mikko Johansson <mikko.johansson2@gmail.com>
2019-11-28 11:10:49 +0200

    finished car-app

--- car-app/src/App.js
+++ car-app/src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 class Car {
   constructor(name) { this.brand = name; }
   present() { return 'I have a ' + this.brand; }
- } 
+}
 
- class SportCar extends Car {
+class SportCar extends Car {
   constructor(name, mod) {
-  super(name);
-  this.model = mod;
+    super(name);
+    this.model = mod;
   }
   show() {
-  return this.present() + ', it is a ' + this.model
+    return this.present() + ', it is a ' + this.model
   }
- } 
+}
 
-function App() {
-  const carList = [
-    {
+//function App() {
+const carList = [
+  {
     mark: 'BMW',
     model: '650i',
     link: 'http://example.com/1'
@@ -31,50 +37,77 @@ function App() {
   },
   {
     mark: 'Audi',
-    model: 'tt', 
+    model: 'tt',
     link: 'http://example.com/2'
   }];
-  
-  let mycar = new SportCar("Ford", "Mustang");
+class App extends Component {
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-	      </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div className="List">
-        <ul>
-          {['Jopo', 'Mopo', 'Moottoripyörä'].map(function(item) {
-            return <li key={item}>{item}</li>
-          })}
-          </ul>
-          <ol>
-          {carList.map(function(item) {
-            return <li>
-              <a className="App-link"
-              href={item.link}
-              target="_blank"
-            rel="noopener noreferrer">
-            {item.mark}: {item.model}
-            </a>
-            </li>
-          })}
-          </ol>
-          <p>{mycar.show()}</p>
+  myCars = [];
+  render() {
+    this.myCars.push(new SportCar("Ford", "Mustang"));
+    this.myCars.push(new SportCar("BMW", "M8"));
+    this.myCars.push(new SportCar("Ferrari", "F50"));
+    return (
+      <Router>
+        <Switch>
+        <Route path="/test">
+          <div>
+            <h2>Test</h2>
           </div>
-      </header>
-    </div>
-  );
+          </Route>
+          <Route path="/">
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Edit <code>src/App.js</code> and save to reload.
+	      </p>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+        </a>
+                <div className="List">
+                  <ul>
+                    {['Jopo', 'Mopo', 'Moottoripyörä'].map(function (item) {
+                      return <li key={item}>{item}</li>
+                    })}
+                  </ul>
+                  <ol>
+                    {carList.filter(item => item.mark === 'BMW').map(function (item) {
+                      return <li key={item}>
+                        <a className="App-link"
+                          href={item.link}
+                          target="_blank"
+                          rel="noopener noreferrer">
+                          {item.mark}: {item.model}
+                        </a>
+                      </li>
+                    })}
+                  </ol>
+                  <ol>
+                    {this.myCars.map(function (item) {
+                      return <li key={item}>
+                        <a className="App-link"
+                          href={item.link}
+                          target="_blank"
+                          rel="noopener noreferrer">
+                          {item.mark}: {item.model}
+                        </a>
+                      </li>
+                    })}
+                  </ol>
+                  {/*<p>{mycar.show()}</p>*/}
+                </div>
+              </header>
+            </div>
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
-
 export default App;
